cd083c31123fe0c4023cbb5e2539b4f90ab32578
Malmas <moustafa_egypte@hotmail.com>
2017-12-05 12:25:43 +0100

    added function to write the status to Firebase

--- js/main.js
+++ js/main.js
@@ -1,14 +1,13 @@
 var firebase = require('firebase');
+//var firebase = require('firebase/database');
+//var firebase = require('firebase/auth');
 var configFile = require('./config.js');
 
-//initialize Firebase
+  firebase.initializeApp(configFile.config); //initialize Firebase
 
-  firebase.initializeApp(configFile.config);
-
-  console.log("API: Tricycle firebase");
-  firebase.auth().signInWithEmailAndPassword(configFile.email, configFile.pass).catch(function(error) {
-    
-    // Handle Errors here.
+  firebase.auth().signInWithEmailAndPassword(configFile.email, configFile.pass).catch(function(error) 
+  {
+  // Handle Errors here.
     var errorCode = error.code;
     var errorMessage = error.message;
   
@@ -17,6 +16,37 @@ var configFile = require('./config.js');
     console.log(errorMessage);
     // ...
   });
+
+  uuid = 'EJyIfXPnAjebmaQXNlAjuqv0BBl1' //uuid of Hans Hoogerwerf
+  var database = firebase.database(); //get reference to the database service
+
+  
+  function changeStatus(uuid) //write the status data to the Firbase Database. 
+  {
+    database.ref('currentstatus/'+ uuid).set( 
+    {
+
+      uuid: uuid,
+      onLocation: false
+
+    });
+  }
+
+  firebase.auth().onAuthStateChanged(function(user) 
+  {
+    if (user!=null) {
+      console.log("Logged in succesfully"); //check if authtication succeeded
+      changeStatusData(uuid);
+    
+    } else {
+      console.log("Not logged in");
+    }
+
+
+  });
+  
+
+  
 
 
 
