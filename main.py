157ea7f71b36950578db9e842773711fcc134c5c
Horofic <ikweetnie123@hotmail.com>
2017-12-07 12:57:55 +0100

    Switched up some logic and added comments.

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ continue_reading = True
 # Capture SIGINT for cleanup when the script is aborted
 def end_read(signal, frame):
     global continue_reading
-    #print "Ctrl+C captured, ending read."
     continue_reading = False
     GPIO.cleanup()
 
@@ -30,22 +29,19 @@ while continue_reading:
     (status, TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
 
     # If a card is found
-    #if status == MIFAREReader.MI_OK:
-        #print "Card detected"
-
-    # Get the UID of the card
-    (status, uid) = MIFAREReader.MFRC522_Anticoll()
+    if status == MIFAREReader.MI_OK:
+        # Get the UID of the card
+        (status, uid) = MIFAREReader.MFRC522_Anticoll() # Method to get uid from cards in sequence
 
     # If we have the UID, continue
     if status == MIFAREReader.MI_OK:
 
-        # Print UID
-        # print "Card read UID: " + str(uid[0]) + "," + str(uid[1]) + "," + str(uid[2]) + "," + str(uid[3])
-
-        key = MIFAREReader.MFRC522_GetKeyFromFile("keyfile")
         # Select the scanned tag
         MIFAREReader.MFRC522_SelectTag(uid)
 
+        # Read the key after we selected the card by uid
+        key = MIFAREReader.MFRC522_GetKeyFromFile("keyfile")
+        
         # Authenticate to the selected tag
         authStatus = MIFAREReader.MFRC522_Auth(MIFAREReader.PICC_AUTHENT1A, 1, key, uid)
         
@@ -54,8 +50,8 @@ while continue_reading:
             sector, data = MIFAREReader.MFRC522_Read(1)
             payload = ''.join(chr(integer) for integer in data)
             
-        print payload # sending data to Nodejs
-        sys.stdout.flush() # clearing the stdout buffer to be ready for the next message
+        print payload # Sending data to Nodejs
+        sys.stdout.flush() # Clearing the stdout buffer to be ready for the next message
 
         # Stop
         MIFAREReader.MFRC522_StopCrypto1()
