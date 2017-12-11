4bce10127103212057c19a99afd8287685bad955
Malmas <moustafa_egypte@hotmail.com>
2017-12-11 15:01:33 +0100

    Automaticaly check and check out! jeej

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
 var firebase = require('firebase');
 var configFile = require('./config.js');
-var pyshell = require('python-shell');
+//var pyshell = require('python-shell');
 
- firebase.initializeApp(configFile.config); 
+ firebase.initializeApp(configFile.config);  //initialize Firebase
  console.log("Please check in");
- var cardID;
+ var cardID = "EkbgGG7UodDNotGb";
+ //var onLocation;
+ initializeAndAuthenticate(); //manual checking
+ /*
  rfid = new pyshell('main.py');
  rfid.on('message', function(message)
 
@@ -14,10 +17,10 @@ var pyshell = require('python-shell');
   initializeAndAuthenticate(); 
 
  });
-
+*/
+var database = firebase.database(); //get reference to the database service
 function initializeAndAuthenticate() 
 {
-  //initialize Firebase
   firebase.auth().signInWithEmailAndPassword(configFile.email, configFile.pass).catch(function(error)
   { // Handle Errors here.
     var errorCode = error.code;
@@ -30,38 +33,7 @@ function initializeAndAuthenticate()
  console.log("Authenticating...");
  validiateAuthtentication();
 }
- // cardID = 'EkbgGG7UodDNotGb' //cardID of Hans Hoogerwerf
- var database = firebase.database(); //get reference to the database service
 
- function checkIfCardIsActive(cardID) 
-{
-  database.ref('cardinfo/' + cardID).once('value').then(function(snapshot)
-  
-  {
-    var cardStatus = (snapshot.val().status);
-    var uuid = (snapshot.val().uuid);
-    console.log(cardStatus);
-    if (cardStatus !== 'active') //check if card is activated by the administrator
-    {
-      throw new Error('Card is not active!');
-    } 
-
-    else 
-    {
-      changeStatus(uuid); //send if user is checked in or checked out
-      console.log("Checked in succesfully");  
-    }
-  });
-}
-  function changeStatus(uuid) //write the status data to the Firbase Database. 
-  {
-    database.ref('currentstatus/'+ uuid).set( 
-    {
-      onLocation: true,
-      uuid: uuid
-      
-    });
-  }
 function validiateAuthtentication()
 { 
   console.log("Validating Authentication...");
@@ -70,6 +42,7 @@ function validiateAuthtentication()
     if (user!=null)  
     {
       console.log("Logged in succesfully"); //check if authtication succeeded
+      //checkInOrCheckOut();
       checkIfCardIsActive(cardID);
       
     } else 
@@ -78,4 +51,38 @@ function validiateAuthtentication()
      }
   });
 }
+ 
+ function checkIfCardIsActive(cardID) 
+{
+  database.ref('cardinfo/' + cardID).once('value').then(function(snapshot)
+  {
+    var cardStatus = (snapshot.val().status);
+    var uuid = (snapshot.val().uuid);
+    //console.log(cardStatus);
+    if (cardStatus !== 'active') //check if card is activated by the administrator
+    {
+      throw new Error('Card is not active!');
+    } 
 
+    else 
+    {
+      changeStatus(uuid);
+      //checkInOrCheckOut(uuid); //send if user is checked in or checked out
+      console.log("Checked in succesfully");  
+    }
+  });
+}
+
+function changeStatus(uuid) //write the status data to the Firbase Database. 
+{
+  database.ref('currentstatus/' + uuid).once('value').then(function(snapshot) 
+  {
+    var onLocation = !(snapshot.val().onLocation);
+      database.ref('currentstatus/'+ uuid).set( 
+      {
+        onLocation: onLocation,
+        uuid: uuid //for set function it is mandotory to write to all database values even if it does'nt change    
+      });
+  });
+  console.log("changed status");
+}
