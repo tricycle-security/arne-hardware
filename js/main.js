6c9be820bc96a8e3424c373ca7ee6b9ccb2208d6
Malmas <moustafa_egypte@hotmail.com>
2017-12-11 09:55:38 +0100

    Implemented RFID listener in code

--- js/main.js
+++ js/main.js
@@ -1,67 +1,80 @@
 var firebase = require('firebase');
-//var firebase = require('firebase/database');
-//var firebase = require('firebase/auth');
 var configFile = require('./config.js');
+var pyshell = require('python-shell');
 
-  firebase.initializeApp(configFile.config); //initialize Firebase
+firebase.initializeApp(configFile.config); 
+var uuid;
+ rfid = new pyshell('main.py');
 
-  firebase.auth().signInWithEmailAndPassword(configFile.email, configFile.pass).catch(function(error) 
-  {
-  // Handle Errors here.
+ rfid.on('message', function(message)
+
+ {
+  uuid = message; 
+  console.log("CardID:" + uuid);
+  initializeAndAuthenticate(); 
+
+ });
+
+function initializeAndAuthenticate() 
+{
+  //initialize Firebase
+  firebase.auth().signInWithEmailAndPassword(configFile.email, configFile.pass).catch(function(error)
+  { // Handle Errors here.
     var errorCode = error.code;
     var errorMessage = error.message;
-  
+
     console.log("authError Here:");
     console.log(errorCode);
     console.log(errorMessage);
-    // ...
   });
-
-  uuid = 'EJyIfXPnAjebmaQXNlAjuqv0BBl1' //uuid of Hans Hoogerwerf
+console.log("Authenticating...");
+validiateAuthtentication();
+}
+ // uuid = 'EkbgGG7UodDNotGb' //uuid of Hans Hoogerwerf
   var database = firebase.database(); //get reference to the database service
 
-  function checkIfUserIsResponder(uuid) 
+  function checkIfCardIsActive(uuid) 
 {
-
-  database.ref('userinfo/' + 'userstatus/' + uuid + '/responder').once('value').then(function(snapshot)
+  database.ref('cardinfo/' + uuid + '/status').once('value').then(function(snapshot)
   {
-    var responder = (snapshot.val());
-    if (responder !== true) 
+    var uuidcheck = (snapshot.val());
+    console.log(uuidcheck);
+    if (uuidcheck !== 'active')
     {
-     throw new Error('User is not a responder'); // This is messy I know will make catch and try block. 
-     console.log("You are not a user!");
-    }
-
+      throw new Error('Card is not active!');
+    } 
+    
     else 
     {
       changeStatus(uuid); //send if user is checked in or checked out
-      console.log("Checked in succesfully");
+      console.log("Checked in succesfully");  
     }
   });
-};
+}
 
   function changeStatus(uuid) //write the status data to the Firbase Database. 
   {
     database.ref('currentstatus/'+ uuid).set( 
     {
-
       uuid: uuid,
       onLocation: false
       
     });
   }
-
+function validiateAuthtentication(uuid)
+{ 
+  console.log("Validating Authentication...");
   firebase.auth().onAuthStateChanged(function(user) 
   {
-    if (user!=null)   {
+    if (user!=null)  
+    {
       console.log("Logged in succesfully"); //check if authtication succeeded
-      checkIfUserIsResponder(uuid);
+      checkIfCardIsActive();
       
-    
-    } else {
+    } else 
+     {
       console.log("Not logged in");
-    }
+     }
   });
-
-
+}
 
