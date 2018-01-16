305759f08308e47ccf7b9fbcb64aa70614c39f89
Malmas <moustafa_egypte@hotmail.com>
2018-01-16 13:51:50 +0100

    changed index.html path

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ var http = require('http');
 var fs = require('fs');
 
 var server = http.createServer(function(req, res) {
-    fs.readFile('/html/index.html', 'utf-8', function(error, content) {
+    fs.readFile('index.html', 'utf-8', function(error, content) {
         res.writeHead(200, {"Content-Type": "text/html"});
         res.end(content);
     });
