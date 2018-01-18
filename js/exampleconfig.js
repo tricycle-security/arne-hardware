0e499595a2254b10c530c912e711a53dbc91195b
Malmas <moustafa_egypte@hotmail.com>
2018-01-18 11:32:23 +0100

    added example config file

---
+++ js/exampleconfig.js
@@ -0,0 +1,12 @@
+var config = { // Put here your Firebase Database Information.
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
+  }; 
+
+  module.exports.config= config;
+  module.exports.email = "your e-mail here";
+  module.exports.pass = "your password here"; 
